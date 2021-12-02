fb39966c424eda808f06c8db710292503560243b
JO3QMA <g.dark.matter@gmail.com>
2021-12-03 04:00:27 +0900

    関数内であっちゃこっちゃ関数呼び出しまくりで気持ち悪かったので、少しきれいにした。

--- core/twitter.rb
+++ core/twitter.rb
@@ -9,43 +9,38 @@ class TwitterClient
     @api_keys_path = './api_keys.yml'
   end
 
-  def check_api_key_file
-    # APIキーが書かれたファイルが有るか確認
-    if File.exist?(@api_keys_path)
-      # ある場合はAPIキーを読み込む
-      load_api_keys
-    else
-      # ない場合は、APIキーを取得して、ファイルに書き込む
-      input_ck_cs
-    end
-  end
-
   def load_api_keys
     # APIキーを読み込む
     api_keys = YAML.load_file(@api_keys_path)
-    # APIキーを読み込んだが、AT,ATSがない場合は、APIキーを取得して書き込む
-    if !api_keys['AccessToken'].nil? || !api_keys['AccessTokenSecret'].nil?
-      authrize_client(api_keys)
+    if !api_keys['Consumer_Key'].nil? && !api_keys['Consumer_Secret_Key'].nil? && !api_keys['Access_Token'].nil? && !api_keys['Access_Token_Secret'].nil?
+      # CK, CS, AT, ATS全てある場合 ->  クライアントを作成する。
+      create_client(api_keys)
+    elsif !api_keys['Consumer_Key'].nil? && !api_keys['Consumer_Secret_Key'].nil?
+      # CK, CSのみある場合 ->  AT,ATSを取得して、ファイルに書き込む。
+      api_keys = get_api_keys(api_keys)
+      save_api_keys(api_keys)
     else
-      get_api_keys
+      # CK, CS, AT, ATS全てがない場合 -> CK,CSを入力してもらう。
+      api_keys = input_ck_cs
+      api_keys = get_api_keys(api_keys)
+      save_api_keys(api_keys)
     end
-    authrize_client(api_keys)
   end
 
-  def authrize_client(api_keys)
+  def create_client(api_keys)
     # 読み込んだAPIキーを使ってTwitterクライアントを作成
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = api_keys['API_Key']
-      config.consumer_secret     = api_keys['API_Secret_Key']
+      config.consumer_key        = api_keys['Consumer_Key']
+      config.consumer_secret     = api_keys['Consumer_Secret_Key']
       config.access_token        = api_keys['Access_Token']
       config.access_token_secret = api_keys['Access_Token_Secret']
     end
   end
 
   def get_api_keys(api_keys)
-    # APIキーを取得する
-    ck = api_keys['API_Key']
-    cs = api_keys['API_Secret_Key']
+    # CK, CSのみある場合、AT,ATSを取得して、ファイルに書き込む。
+    ck = api_keys['Consumer_Key']
+    cs = api_keys['Consumer_Secret_Key']
     consumer = OAuth::Consumer.new ck, cs, site: 'https://api.twitter.com'
 
     request_token = consumer.get_request_token
@@ -54,11 +49,16 @@ class TwitterClient
     STDERR.print 'PINコード: '
 
     access_token = request_token.get_access_token oauth_verifier: gets.chomp
-    puts "Access Token : #{access_token.token}"
-    puts "Access Secret: #{access_token.secret}"
     at = access_token.token
     ats = access_token.secret
-    save_api_keys(ck, cs, at, ats)
+    puts "Access Token : #{at}"
+    puts "Access Secret: #{ats}"
+    {
+      'Consumer_Key' => ck,
+      'Consumer_Secret_Key' => cs,
+      'Access_Token' => access_token.token,
+      'Access_Token_Secret' => access_token.secret
+    }
   end
 
   def input_ck_cs
@@ -69,23 +69,31 @@ class TwitterClient
     ck = gets.chomp
     STDERR.print 'CS: '
     cs = gets.chomp
-    api_keys = {
-      'API_Key' => ck,
-      'API_Secret_Key' => cs
+    {
+      'Consumer_Key' => ck,
+      'Consumer_Secret_Key' => cs
     }
-    get_api_keys(api_keys)
   end
 
-  def save_api_keys(ck = nil, cs = nil, at = nil, ats = nil)
+  def save_api_keys(api_keys)
     # APIキーを保存する
-    api_keys = {
-      'API_Key' => ck,
-      'API_Secret_Key' => cs,
-      'Access_Token' => at,
-      'Access_Token_Secret' => ats
-    }
     File.open(@api_keys_path, 'w') do |f|
       f.write api_keys.to_yaml
+    end
+  end
+
+  def auth_sequence
+    # 認証シークエンス
+
+    # APIキーが書かれたファイルが有るか確認
+    if File.exist?(@api_keys_path)
+      # ある場合はAPIキーを読み込む
+      load_api_keys
+    else
+      # ない場合は、APIキーを取得して、ファイルに書き込む
+      api_keys = input_ck_cs
+      api_keys = get_api_keys(api_keys)
+      save_api_keys(api_keys)
     end
   end
 
