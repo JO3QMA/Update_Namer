324fd15f630c5f743ed9700f02fe3bd567a9164a
JO3QMA <g.dark.matter@gmail.com>
2021-12-02 10:58:49 +0900

    パーサーのクラス。今回は丁寧に書きたい。

---
+++ core/parser.rb
@@ -0,0 +1,15 @@
+# リプライパーサー
+class ReplieParser
+  def initialize
+    # 初期化
+  end
+
+  def parse(reply)
+    # リプライをパースする
+  end
+
+  def separation_reply_source(reply)
+    # リプライのソースを分割する
+    # @username @username... Messageという形式になる場合が多い
+  end
+end
